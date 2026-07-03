2862438fce11540107e663ef1f4d23b5f01d3dc2
RizalSiStudent <2331730105@student.polinema.ac.id>
2026-07-03 19:03:42 +0700

    vercel.json & api/index.php for vercel deployment

---
+++ api/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Laravel - Vercel Serverless Bridge
+ * Kode ini berfungsi untuk menjembatani dan mengarahkan seluruh request 
+ * dari Vercel Serverless Function ke file index.php utama milik Laravel.
+ */
+
+require __DIR__ . '/../public/index.php';
